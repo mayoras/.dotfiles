98e9afb4d01cc254c3bc2cbc1fbbe43f6fbc7197
Cesar Mayora <cesarmay@protonmail.com>
2024-09-26 19:50:44 +0200

    conda out of my life

--- fish/config.fish
+++ fish/config.fish
@@ -10,11 +10,10 @@ set -gx DENO_INSTALL $HOME/.deno
 fish_add_path $DENO_INSTALL/bin
 # <<< deno initialize <<<
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/cam/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
 # Aliases
 alias lah="ls -lah"
 alias vi=nvim
+
+# Wasmer
+export WASMER_DIR="/home/cam/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
